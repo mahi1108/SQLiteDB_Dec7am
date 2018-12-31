010d0a486c72c005be76b0703910128bd0b8ccdb
maheshthippala <mahesh.cubex@gmail.com>
2018-12-31 08:47:59 +0530

    Updated .....

--- app/src/main/java/cubex/mahesh/sqlitedb_dec7am/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/sqlitedb_dec7am/MainActivity.kt
@@ -56,9 +56,37 @@ class MainActivity : AppCompatActivity() {
             /* Cursor query(String table, String[] columns,
             String selection, String[] selectionArgs,
             String groupBy, String having, String orderBy)*/
-         var c:Cursor =    dBase.query("employee",null,
+    /*     var c:Cursor =    dBase.query("employee",
+             arrayOf("eid","ename","edesig","edept"),
              null, null,null,
-             null,null)
+             null,null) */
+    /*        var c:Cursor =    dBase.query("employee",
+                null,
+                "eid=? and edept=?",
+                arrayOf(et1.text.toString(),et4.text.toString()),
+                null,
+                null,null) */
+
+       /*     var c:Cursor =    dBase.query("employee",
+                null,
+                null,
+                null,
+                "ename",
+                null,null) */
+
+        /*    var c:Cursor =    dBase.query("employee",
+                null,
+                null,
+                null,
+                "eid",
+                "eid>125",null) */
+
+          var c:Cursor =    dBase.query("employee",
+                null,
+                null,
+                null,
+                null,
+                null,"eid desc")
 // Context context, int layout, Cursor c, String[] from, int[] to, int flags
  var from:Array<String> = arrayOf("eid","ename","edesig","edept")
  var to:IntArray = intArrayOf(R.id.eid,R.id.ename,R.id.edesig,R.id.edept)
@@ -67,9 +95,50 @@ class MainActivity : AppCompatActivity() {
               R.layout.indiview,c,from,to,0)
   lview.adapter  = myadapter
 
-        }
+        } // Read
 
 
+        update.setOnClickListener {
+ /* int update(String table, ContentValues values,
+      String whereClause, String[] whereArgs) */
+            var cv = ContentValues( )
+            cv.put("ename",et2.text.toString())
+            cv.put("edesig",et3.text.toString())
+      var status =       dBase.update("employee",cv,
+                "eid=?",
+                arrayOf(et1.text.toString()))
+
+            if(status!=0){
+                Toast.makeText(this@MainActivity,
+                    "Data is Updated..",
+                    Toast.LENGTH_LONG).show()
+                et1.setText(""); et2.setText("")
+                et3.setText(""); et4.setText("")
+            }else{
+                Toast.makeText(this@MainActivity,
+                    "Data Updation  Failed..",
+                    Toast.LENGTH_LONG).show()
+            }
+        } // Update
+
+
+        delete.setOnClickListener {
+ // int delete(String table, String whereClause, String[] whereArgs)
+  var status =   dBase.delete("employee",
+                            "eid=?",
+                        arrayOf(et1.text.toString()))
+            if(status!=0){
+                Toast.makeText(this@MainActivity,
+                    "Data is Deleted..",
+                    Toast.LENGTH_LONG).show()
+                et1.setText(""); et2.setText("")
+                et3.setText(""); et4.setText("")
+            }else{
+                Toast.makeText(this@MainActivity,
+                    "Data Deletion  Failed..",
+                    Toast.LENGTH_LONG).show()
+            }
+        } //delete
 
 
     } // onCreate( )
